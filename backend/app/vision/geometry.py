fd017aca3ae3875df80c2abbc87e418e174a06eb
YOSSHMI NAIR <yosshmi04@gmail.com>
2026-09-22 16:10:01 +0530

    feat: add reusable vision geometry helpers

---
+++ backend/app/vision/geometry.py
@@ -0,0 +1,31 @@
+from collections.abc import Sequence
+
+Point = tuple[float, float]
+
+
+def point_in_polygon(point: Point, polygon: Sequence[Point]) -> bool:
+    if len(polygon) < 3:
+        return False
+
+    x, y = point
+    inside = False
+    j = len(polygon) - 1
+
+    for i in range(len(polygon)):
+        xi, yi = polygon[i]
+        xj, yj = polygon[j]
+
+        intersects = ((yi > y) != (yj > y)) and (
+            x < (xj - xi) * (y - yi) / ((yj - yi) or 1e-12) + xi
+        )
+        if intersects:
+            inside = not inside
+        j = i
+
+    return inside
+
+
+def side_of_line(point: Point, start: Point, end: Point) -> float:
+    return (end[0] - start[0]) * (point[1] - start[1]) - (
+        end[1] - start[1]
+    ) * (point[0] - start[0])
